7da4d695e02fe6acbaadcafd220de224a0d45f28
Angel711 <arubio15@alumnos.uaq.mx>
2024-04-20 02:32:02 -0600

    DEV: Add functionality for dynamics cards

--- project_logistic/page/urls.py
+++ project_logistic/page/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from .views import product_list
+
+urlpatterns = [
+    path('products/', product_list, name='product_list'),
+]
 
 urlpatterns = [
     path('prueba/', views.Prueba, name='Prueba')
--- project_logistic/page/views.py
+++ project_logistic/page/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Example
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'tarjeta.html', {'products': products})
 
 # Create your views here.
 
