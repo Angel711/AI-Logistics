d9ea8dc604a35cb5be935daf5039f855af9ff1f1
Axel FCG <axfervagar@gmail.com>
2024-04-20 03:09:16 -0600

    Auto stash before merge of "main" and "origin/main"

--- project_logistic/page/urls.py
+++ project_logistic/page/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-    path('prueba/', views.Prueba, name='Prueba')
+    path('login/', views.Login, name='Login'),
+    path('principal/', views.Principal, name='Principal'),
+    path('card/', views.Card, name='Card'),
+    path('addtemplate/', views.Addtemplate, name='Addtemplate'),
+    path('sellers/', views.Sellers, name='Sellers')
 ]
--- project_logistic/page/views.py
+++ project_logistic/page/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Example
-from django.shortcuts import render
 from .models import Product
 
 def product_list(request):
@@ -9,8 +8,17 @@ def product_list(request):
     return render(request, 'tarjeta.html', {'products': products})
 
 # Create your views here.
+def Login(request):
+    return render(request, 'login.html', {})
 
-def Prueba(request):
-    exame = Example(headling='este es un texto')
-    exame.save()
-    return HttpResponse('Datos guardados')
+def Principal(request):
+    return render(request, 'principal.html', {})
+
+def Card(request):
+    return render(request, 'card.html', {})
+
+def Addtemplate(request):
+    return render(request, 'addtemplate.html', {})
+
+def Sellers(request):
+    return render(request, 'sellers.html', {})
